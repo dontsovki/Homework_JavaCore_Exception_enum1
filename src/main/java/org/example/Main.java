9dd6f44ab00e35a9554696d7166398bb31f8fce2
Artem.don <dontsovki@gmail.com>
2023-10-30 22:14:39 +0300

    123456

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,26 @@
 package org.example;
 
-import org.example.Pacadge.Task1;
+import org.example.Pacadge.*;
 
 public class Main {
     public static void main(String[] args) {
-        Task1.DayOfWeek[] days = Task1.DayOfWeek.values();
-        for (Task1.DayOfWeek s : days){
-            System.out.printf("%s", s);
+        Task_values_.DayOfWeek[] days = Task_values_.DayOfWeek.values();
+        for (Task_values_.DayOfWeek s : days){
+            System.out.printf("%s", s); // Вывод всех полей перечесления
+        }
+        Task_ordinal_.DayOfWeeker current = Task_ordinal_.DayOfWeeker.FRIDAY;
+        System.out.println(current);
+        System.out.println(current.ordinal()); //Возвращает индекс элемента
+        System.out.println(Task_class_.Color.Red.getCod());//Через конструктор передается значение пользовательского поля
+        System.out.println(Task_class_.Color.Green.getCod());//Через конструктор передается значение пользовательского поля
+
+        Task_class_.Color[] pound = Task_class_.Color.values();
+        for (Task_class_.Color d : pound){
+            System.out.printf(d.getCod()); // Вывод всех полей перечесления
+
+        }
+
+
+
     }
-}}
+}
